c5c94eaf944bb59c211f30c25ba5fa510a8b3ad6
Barry Allard <barry.allard@gmail.com>
2014-03-15 20:04:56 -0700

    0.0.2 - correct uk, languages, cool flags
    Signed-off-by: Barry Allard <barry.allard@gmail.com>

--- lib/svg-flags-rails/version.rb
+++ lib/svg-flags-rails/version.rb
@@ -1,4 +1,4 @@
 module SvgFlagsRails
-  VERSION = '0.0.1' # https://github.com/koppi/iso-country-flags-svg-collection@285c21fb080c3ee132f494afa4f79741903b8c6c
+  VERSION = '0.0.2' # https://github.com/koppi/iso-country-flags-svg-collection@285c21fb080c3ee132f494afa4f79741903b8c6c
 end
 
