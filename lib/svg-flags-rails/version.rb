d4197a57abff1af00b95b2d396611bcc4746cc34
Barry Allard <barry.allard@gmail.com>
2017-04-23 21:12:47 -0700

    version bump to beta3
    Signed-off-by: Barry Allard <barry.allard@gmail.com>

--- lib/svg-flags-rails/version.rb
+++ lib/svg-flags-rails/version.rb
@@ -1,4 +1,4 @@
 module SvgFlagsRails
-  VERSION = '1.0.0-beta2' # https://github.com/koppi/iso-country-flags-svg-collection@285c21fb080c3ee132f494afa4f79741903b8c6c
+  VERSION = '1.0.0-beta3' # https://github.com/koppi/iso-country-flags-svg-collection@285c21fb080c3ee132f494afa4f79741903b8c6c
 end
 
