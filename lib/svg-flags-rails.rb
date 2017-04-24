992dcf1405c1c08890fdeecaf3acaa00b83b5ae1
Barry Allard <barry.allard@gmail.com>
2017-04-23 21:11:49 -0700

    closes #10
    Signed-off-by: Barry Allard <barry.allard@gmail.com>

--- lib/svg-flags-rails.rb
+++ lib/svg-flags-rails.rb
@@ -3,7 +3,9 @@ require 'svg-flags-rails/version'
 module SvgFlagsRails
   module Rails
     class Engine < ::Rails::Engine
+      initializer 'svg-flags-rails.assets.precompile' do |app|
+        app.config.assets.precompile << 'country-*/*.svg'
+      end
     end
   end
 end
-
